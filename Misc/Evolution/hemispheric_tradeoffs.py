ab665be2f682799870f134c07c153f96b830aa00
Beren Millidge <bmillidgework@gmail.com>
2018-08-30 22:15:19 +0100

    Add files via upload

--- Misc/Evolution/hemispheric_tradeoffs.py
+++ Misc/Evolution/hemispheric_tradeoffs.py
@@ -2,16 +2,20 @@ from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 
-E0 = 10
+E0 = 5
+alpha = 1.
 
 def tradeoff(E, N):
-	return E - (E / np.sqrt(N)) - N
+	return E - (E / np.sqrt(N )) - N
 
-def tradeoff2(E,N):
-	return -(E / np.sqrt(N)) - N
+def tradeoff2(E,N, alpha):
+	return -(E / np.sqrt(N )) - (alpha * N)
 
 
-xs = [i for i in range(50)]
-vals = [tradeoff2(E0, N) for N in xs]
+xs = [int(i) for i in range(10)]
+vals = [tradeoff2(E0, N, alpha) for N in xs]
 plt.plot(xs, vals)
+plt.title("Hemispheric tradeoffs - accuracy vs metabolic cost")
+plt.xlabel("Number of hemispheres")
+plt.ylabel("Usefulness score")
 plt.show()
